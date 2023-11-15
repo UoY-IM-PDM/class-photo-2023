5ab1e6196bde32db9c918222399cdacd9e8057f5
ace01603 <50892754+ace01603@users.noreply.github.com>
2023-11-15 12:44:25 +0000

    starter code

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(1000, 600);
+}
+
+function draw() {
+    background(255);
+}
+
+/**
+ * INSTRUCTIONS - DO NOT DELETE
+ * Add your code for creating your character below (either a class or function). To avoid conflicting names, 
+ * include your GitHub username (or your uni username) in the function or class name.
+ * 
+ * Finally, call your function / class in draw to add your character to the photo
+ */
